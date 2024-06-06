8078a390f7c242f448263f98b509443353670faf
Shabab Salehin <auddhayashabab@gmail.com>
2024-06-06 16:49:24 +0600

    phone validation added

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted' => 'The :attribute must be accepted.',
+    'active_url' => 'The :attribute is not a valid URL.',
+    'after' => 'The :attribute must be a date after :date.',
+    // ... other validation messages
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'phone' => [
+            'regex' => 'The phone number must be a valid format (e.g., 01617499123, +8801347368467, +951474154).',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader-friendly such as E-Mail Address instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [],
+
+];
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends("layouts.layout")
+
+@section("title", "Dashboard Page")
+
+@section("content")
+
+<h2>Hi "{{ auth()->user()->name }}", Welcome to the Dashboard</h2>
+<p>This is the personal Dashboard.</p>
+
+@endsection
